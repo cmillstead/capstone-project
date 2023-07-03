40b48ad20f75d076a3b9bae9b151045e7acbc237
Cevin Millstead <cevinm@gmail.com>
2023-07-02 23:13:02 -0700

    18 navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,44 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';    
+import { useDispatch } from 'react-redux';
 import config from '../config.json';
-import { 
-  loadProvider, 
-  loadNetwork, 
+
+import {
+  loadProvider,
+  loadNetwork,
   loadAccount,
   loadTokens,
   loadExchange
 } from '../store/interactions';
 
+import Navbar from './Navbar'
+
 function App() {
   const dispatch = useDispatch();
 
-  const loadBlockchainData = async () => { 
+  const loadBlockchainData = async () => {
     // connect Ethers to blockchain
-    const provider = await loadProvider(dispatch);
-    
-    // fetch current network's chainId
+    const provider = loadProvider(dispatch);
+
+    // fetch current network's chainId (e.g. hardhat: 31337, goerlie: 5)
     const chainId = await loadNetwork(provider, dispatch);
-    
-    // fetch current account and balance from metamask
-    loadAccount(provider, dispatch);
+
+    // reload page when network changes
+    window.ethereum.on('chainChanged', () => {
+      window.location.reload();
+    })
+
+    // fetch current account & balance from Metamask when changed
+    window.ethereum.on('accountsChanged', () => {
+      loadAccount(provider, dispatch);
+    })
 
     // load token smart contracts
+    console.log(`Loading tokens for chainId: ${chainId}`);
     const DApp = config[chainId].DApp;
     const mETH = config[chainId].mETH;
-    await loadTokens(provider, [DApp.address, mETH.address], dispatch) ;
+    await loadTokens(provider, [DApp.address, mETH.address], dispatch);
 
-    // exchange smart contract
+    // load exchange smart contract
     const exchangeConfig = config[chainId].exchange;
     await loadExchange(provider, exchangeConfig.address, dispatch);
   }
@@ -39,7 +50,7 @@ function App() {
   return (
     <div>
 
-      {/* Navbar */}
+      <Navbar />
 
       <main className='exchange grid'>
         <section className='exchange__section--left grid'>
