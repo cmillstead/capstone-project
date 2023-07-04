a66f7e013a1a0dc492e45a8e90943744094b94be
Cevin Millstead <cevinm@gmail.com>
2023-07-03 20:47:36 -0700

    20 deposits pt 1

--- src/components/App.js
+++ src/components/App.js
@@ -7,11 +7,13 @@ import {
   loadNetwork,
   loadAccount,
   loadTokens,
-  loadExchange
+  loadExchange,
+  subscribeToEvents
 } from '../store/interactions';
 
 import Navbar from './Navbar'
 import Markets from './Markets';
+import Balance from './Balance';
 
 
 function App() {
@@ -35,14 +37,16 @@ function App() {
     })
 
     // load token smart contracts
-    console.log(`Loading tokens for chainId: ${chainId}`);
     const DApp = config[chainId].DApp;
     const mETH = config[chainId].mETH;
     await loadTokens(provider, [DApp.address, mETH.address], dispatch);
 
     // load exchange smart contract
     const exchangeConfig = config[chainId].exchange;
-    await loadExchange(provider, exchangeConfig.address, dispatch);
+    const exchange = await loadExchange(provider, exchangeConfig.address, dispatch);
+
+    // Listen to events
+    subscribeToEvents(exchange, dispatch);
   }
 
   useEffect(() => {
@@ -59,7 +63,7 @@ function App() {
 
           <Markets />
 
-          {/* Balance */}
+          <Balance />
 
           {/* Order */}
 
--- src/store/interactions.js
+++ src/store/interactions.js
@@ -1,7 +1,6 @@
 import { ethers } from 'ethers'
 import TOKEN_ABI from '../abis/Token.json';
 import EXCHANGE_ABI from '../abis/Exchange.json';
-import { provider } from './reducers';
 
 export const loadProvider = (dispatch) => {
     const connection = new ethers.providers.Web3Provider(window.ethereum);
@@ -33,21 +32,66 @@ export const loadAccount = async (provider, dispatch) => {
 
 export const loadTokens = async (provider, addresses, dispatch) => {
     let token, symbol;
-
-    token = new ethers.Contract(addresses[0], TOKEN_ABI, provider)
+  
+    token = new ethers.Contract(addresses[0], TOKEN_ABI, provider);
     symbol = await token.symbol();
     dispatch({ type: 'TOKEN_1_LOADED', token, symbol });
-
+  
     token = new ethers.Contract(addresses[1], TOKEN_ABI, provider);
     symbol = await token.symbol();
     dispatch({ type: 'TOKEN_2_LOADED', token, symbol });
-
+  
     return token;
-}
+  }
 
 export const loadExchange = async (provider, address, dispatch) => {    
     const exchange = new ethers.Contract(address, EXCHANGE_ABI, provider);
     dispatch({ type: 'EXCHANGE_LOADED', exchange });
 
     return exchange;
+}
+
+export const subscribeToEvents = (exchange, dispatch) => {
+    exchange.on('Deposit', (token, user, amount, balance, event) => {
+      dispatch({ type: 'TRANSFER_SUCCESS', event });
+    });
+  }
+
+// ------------------------------------------------------------------------------
+// LOAD USER BALANCES (WALLET & EXCHANGE BALANCES)
+
+export const loadBalances = async (exchange, tokens, account, dispatch) => {
+    let balance = ethers.utils.formatUnits(await tokens[0].balanceOf(account), 18);
+    dispatch({ type: 'TOKEN_1_BALANCE_LOADED', balance });
+  
+    balance = ethers.utils.formatUnits(await exchange.balanceOf(tokens[0].address, account), 18);
+    dispatch({ type: 'EXCHANGE_TOKEN_1_BALANCE_LOADED', balance });
+  
+    balance = ethers.utils.formatUnits(await tokens[1].balanceOf(account), 18);
+    dispatch({ type: 'TOKEN_2_BALANCE_LOADED', balance });
+  
+    balance = ethers.utils.formatUnits(await exchange.balanceOf(tokens[1].address, account), 18);
+    dispatch({ type: 'EXCHANGE_TOKEN_2_BALANCE_LOADED', balance });
+  }
+
+// ------------------------------------------------------------------------------
+// TRANSFER CASES (DEPOSIT & WITHDRAWS)
+
+export const transferTokens = async (provider, exchange, transferType, token, amount, dispatch) => {
+    let transaction;
+
+    dispatch({ type: 'TRANSFER_REQUEST', status: true });
+
+    try {
+        const signer = await provider.getSigner();
+        const amountToTransfer = ethers.utils.parseUnits(amount.toString(), 18);
+    
+        transaction = await token.connect(signer).approve(exchange.address, amountToTransfer);
+        await transaction.wait();
+        transaction = await exchange.connect(signer).depositToken(token.address, amountToTransfer);
+        await transaction.wait();
+    } catch (error) {
+        dispatch({ type: 'TRANSFER_FAIL' });
+    }
+
 }
